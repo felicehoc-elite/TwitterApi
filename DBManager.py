ab43bc8b6723a005a234e22b72f3c92f955d5d9f
Felix Arnold <felicehoc.0206@gmail.com>
2020-12-15 22:36:51 +0100

    Fixing mysql database connection

--- DBManager.py
+++ DBManager.py
@@ -5,18 +5,16 @@ import mysql.connector
 
 class MySqlDBManager:
 
-
-    def __init__(self, host, user, password, database) -> None:
+    def __init__(self, host, user, password, database):
         self.host = host
         self.user = user
         self.password = password
         self.database = database
         self.connection = None
 
-
     def connect(self):
-        self.connection = mysql.connector.connect(self.host, self.user, self.password, self.database)
-        self.connection.autocommit = False
+         self.connection = mysql.connector.connect(host = self.host, user = self.user, password = self.password, database = self.database)
+         self.connection.autocommit = False
 
     def close(self):
         self.connection.close()
