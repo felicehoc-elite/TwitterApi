3ed3f9c4d5a3613a9e6d8fb9a4ebf94d5204118a
Felix Arnold <felicehoc.0206@gmail.com>
2020-12-16 16:40:21 +0100

    Implementing CrudRepos for the model (Insert)

---
+++ CrudRepositories.py
@@ -0,0 +1,103 @@
+import DBManager
+from Model import *
+
+
+class TweetRepository(DBManager.MySqlDBManager):
+    def insert(self, tweet: Tweet):
+        self.connect()
+        cursor = self.connection.cursor()
+        sql = "INSERT INTO Tweet (tweet_id, created_at, lang, possibly_sensitive, source, " \
+              "text, like_count, quote_count, reply_count, retweet_count) " \
+              "VALUES (%s, %s,%s, %s,%s, %s,%s, %s,%s, %s)"
+        val = (tweet.tweet_id,tweet.created_at, tweet.lang, tweet.possibly_sensitive, tweet.source,
+               tweet.text, tweet.like_count, tweet.quote_count, tweet.reply_count, tweet.retweet_count)
+        cursor.execute(sql, val)
+        self.close()
+
+
+class AnnotationRepository(DBManager.MySqlDBManager):
+    def insert(self, annotation: Annotation):
+        self.connect()
+        cursor = self.connection.cursor()
+        sql = "INSERT INTO Annotation (annotation_id, normalized_text, probability)" \
+              "VALUES (%s, %s, %s)"
+        val = (annotation.annotation_id, annotation.normalized_text, annotation.probability)
+        cursor.execute(sql, val)
+        self.close()
+
+
+class HashtagRepository(DBManager.MySqlDBManager):
+    def insert(self, hashtag: Hashtag):
+        self.connect()
+        cursor = self.connection.cursor()
+        sql = "INSERT INTO Hashtag (hashtag_id, tag)" \
+              "VALUES (%s, %s)"
+        val = (hashtag.hashtag_id,hashtag.tag)
+        cursor.execute(sql, val)
+        self.close()
+
+
+class UrlRepository(DBManager.MySqlDBManager):
+    def insert(self, url: Url):
+        self.connect()
+        cursor = self.connection.cursor()
+        sql = "INSERT INTO Url (url_id, description, expanded_url, status, title, unwound_url)" \
+              "VALUES (%s, %s, %s, %s, %s, %s)"
+        val = (url.url_id, url.description, url.expanded_url,url.status, url.title, url.unwound_url)
+        cursor.execute(sql, val)
+        self.close()
+
+
+class ImageRepository(DBManager.MySqlDBManager):
+    def insert(self, image: Image):
+        self.connect()
+        cursor = self.connection.cursor()
+        sql = "INSERT INTO Image (image_id, url, height,width,url_id)" \
+              "VALUES (%s, %s, %s, %s, %s)"
+        val = (image.image_id, image.url, image.height, image.width, image.url_id)
+        cursor.execute(sql, val)
+        self.close()
+
+
+class EntityRepository(DBManager.MySqlDBManager):
+    def insert(self, entity: Entity):
+        self.connect()
+        cursor = self.connection.cursor()
+        sql = "INSERT INTO Entity (tweet_id, annotation_id, hashtag_id, url_id, end, start)" \
+              "VALUES (%s, %s, %s, %s, %s, %s)"
+        val = (entity.tweet_id, entity.annotation_id, entity.hashtag_id, entity.url_id, entity.end, entity.start)
+        cursor.execute(sql, val)
+        self.close()
+
+
+class DomainRepository(DBManager.MySqlDBManager):
+    def insert(self, domain: Domain):
+        self.connect()
+        cursor = self.connection.cursor()
+        sql = "INSERT INTO Domain (domain_id, name, description)" \
+              "VALUES (%s, %s, %s)"
+        val = (domain.domain_id, domain.name, domain.description)
+        cursor.execute(sql, val)
+        self.close()
+
+
+class ContextEntityRepository(DBManager.MySqlDBManager):
+    def insert(self, context_entity: ContextEntity):
+        self.connect()
+        cursor = self.connection.cursor()
+        sql = "INSERT INTO ContextEntity (context_entity_id, name, description)" \
+              "VALUES (%s, %s, %s)"
+        val = (context_entity.context_entity_id, context_entity.name, context_entity.description)
+        cursor.execute(sql, val)
+        self.close()
+
+
+class ContextAnnotationRepository(DBManager.MySqlDBManager):
+    def insert(self, context_annotation: ContextAnnotation):
+        self.connect()
+        cursor = self.connection.cursor()
+        sql = "INSERT INTO ContextAnnotation (context_annotation_id, domain_id, context_entity_id)" \
+              "VALUES (%s, %s, %s)"
+        val = ( context_annotation.context_annotation_id,  context_annotation.domain_id, context_annotation.context_entity_id)
+        cursor.execute(sql, val)
+        self.close()
